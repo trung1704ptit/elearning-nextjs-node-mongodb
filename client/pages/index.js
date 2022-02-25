5b8b708ca9f961145cb820104c15f60677eefb19
Trung Nguyen <trung1704ptit@gmail.com>
2022-02-25 22:01:17 +0700

    init client

---
+++ client/pages/index.js
@@ -0,0 +1,10 @@
+const Index = () => {
+  return (
+    <>
+    <h1 className="jumbotron text-center bg-primary square">Hello world</h1>
+    <p>Welcome to tutorial</p>
+    </>
+  )
+}
+
+export default Index;
---
+++ client/pages/login.js
@@ -0,0 +1,12 @@
+const Login = () => {
+  return (
+    <>
+      <div>
+        <h1 className="jumbotron text-center bg-primary square">Login Page</h1>
+      </div>
+        <p>Welcome to tutorial</p>
+    </>
+  )
+}
+
+export default Login;
