4682068df6e4d08b29d77b2486f79a69c0c6d7ea
Trung Nguyen <trung1704ptit@gmail.com>
2022-02-25 22:03:24 +0700

    update login page

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,10 +1,8 @@
 const Login = () => {
   return (
     <>
-      <div>
-        <h1 className="jumbotron text-center bg-primary square">Login Page</h1>
-      </div>
-        <p>Welcome to tutorial</p>
+    <h1 className="jumbotron text-center bg-primary square">Login Page</h1>
+    <p>Welcome to tutorial</p>
     </>
   )
 }
